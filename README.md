42a6a2f697b8391da16e8ab0eab8c40d14bf98e7
elmiAnass <93386332+elmiAnass@users.noreply.github.com>
2022-10-12 22:34:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-Card_whit_map_listdata
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ekstgf)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+     <div>
+         <h1 className={"display-2 text-center"}></h1>
+    </div>
+  );
+}
